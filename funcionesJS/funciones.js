411762ab50135f14d1e5c3998c9ad925c372e7f7
angelDariaux <angeldariaux@gmail.com>
2021-12-20 20:21:39 -0600

    terminada la primera clase de las funciones

--- funcionesJS/funciones.js
+++ funcionesJS/funciones.js
@@ -18,4 +18,19 @@ calculadora();
 let resultado = calculadora();
 console.log(`Resultado: ${resultado}`);
 
- 
+        //PARAMETROS
+
+const calculadora1 = ( numero1, numero2 ) => { 
+  console.group(`Calculadora N.1, Numeros recibidos: ${numero1} & ${numero2}`);
+    console.log(`Suma: ${numero1 + numero2} `);
+    console.log(`Resta: ${numero1 - numero2} `);
+    console.log(`Multiplicacion: ${numero1 * numero2} `);
+    console.log(`Division: ${numero1 / numero2} `);
+    console.log(`Percentil: ${numero1 % numero2} `);
+  console.groupEnd();
+  return "Hola soy la calculadora 1";
+};
+
+for (let index = 1; index <= 10; index++) {
+  calculadora1(index, 8);
+}
